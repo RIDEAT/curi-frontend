a1f1cb524d1fa0a13e669ba837d639bf3442329c
Kang <rkdalsgur032@unist.ac.kr>
2023-07-29 18:53:48 +0900

    [RID-304][#45][feat] : gtm script

--- apps/demo/app/layout.tsx
+++ apps/demo/app/layout.tsx
@@ -5,6 +5,7 @@ import Script from "next/script";
 import "ui/styles/globals.css";
 
 const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
+const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
 
 export const metadata: Metadata = {
   title: "Curi Board",
@@ -31,7 +32,24 @@ export default function RootLayout({
             gtag('config', '${GA_MEASUREMENT_ID}');
         `}
       </Script>
+      <Script id="gtm-script" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer', '${GTM_ID}');
+      `}
+      </Script>
       <body suppressHydrationWarning={true}>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
         {/* <Toaster /> */}
       </body>
