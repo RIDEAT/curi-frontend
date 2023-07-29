943f2d31e22fce4b8ad83c486a131c9d432b9a65
Kang <rkdalsgur032@unist.ac.kr>
2023-07-29 18:18:36 +0900

    [RID-304][#45][feat] : google tag manager

--- apps/demo/app/layout.tsx
+++ apps/demo/app/layout.tsx
@@ -1,9 +1,16 @@
-"use client";
+import { Metadata } from "next";
+import Script from "next/script";
 
-import { Provider } from "jotai";
-
-import { Toaster } from "ui";
+// import { Toaster } from "ui";
 import "ui/styles/globals.css";
+
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_MEASUREMENT_ID;
+
+export const metadata: Metadata = {
+  title: "Curi Board",
+  description: "새로운 동료의 첫만남을 디자인하세요",
+  viewport: "width=device-width, initial-scale=1",
+};
 
 export default function RootLayout({
   children,
@@ -12,9 +19,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+        `}
+      </Script>
       <body suppressHydrationWarning={true}>
-        <Provider>{children}</Provider>
-        <Toaster />
+        {children}
+        {/* <Toaster /> */}
       </body>
     </html>
   );
