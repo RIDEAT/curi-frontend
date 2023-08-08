eabd0ec7d998d760bd92b7a3db0a31fea0898bd4
Kang <rkdalsgur032@unist.ac.kr>
2023-08-08 18:51:53 +0900

    [RID-357][#45][feat] : landing second page

--- apps/demo/app/page.tsx
+++ apps/demo/app/page.tsx
@@ -14,28 +14,36 @@ import {
   NavigationMenuItem,
   NavigationMenuLink,
   NavigationMenuList,
+  Separator,
 } from "ui";
 
 import { cn } from "ui/lib/utils";
 import { CuriExmapleLogo } from "../components/logos/curi-example-logo";
 import main_ui from "../public/main_ui.png";
 import workflow_sm from "../public/workflow_sm.png";
-import dashboard_sm from "../public/dashboard_sm.png";
+import editor_main from "../public/editor_main.png";
 
-const backgroundStyle = "bg-gradient-to-br from-white to-violet-300";
+const backgroundStyle1 = "bg-gradient-to-br from-white to-violet-300";
+const backgroundStyle2 = "bg-gradient-to-br from-white to-blue-200";
+const backgroundStyle3 = "bg-gradient-to-br from-white to-green-100";
 
 export default function Home() {
   return (
     <>
-      <div className={cn("flex flex-col justify-center", backgroundStyle)}>
-        <LandingNavigation />
-        <main className="flex justify-start items-start">
-          <div className="h-[200vh] flex flex-col gap-2 mr-10">
-            <div className="flex justify-between mt-12 md:mt-32 ml-4 sm:ml-8 md:ml-12">
+      <main>
+        <div className={cn("flex flex-col justify-center")}>
+          <div
+            className={cn(
+              "justify-start items-start flex flex-col gap-2 pb-10 pl-6 sm:pl-0",
+              backgroundStyle1
+            )}
+          >
+            <LandingNavigation />
+            <div className="flex justify-between mt-6 md:mt-20 ml-4 sm:ml-8 md:ml-12 mr-10">
               <LandingHero />
             </div>
-            <div className="flex flex-col gap-10">
-              <div className="hidden sm:block ml-7">
+            <div className="flex flex-col gap-10 ml-3 sm:ml-7 mr-10">
+              <div className="mt-8 sm:mt-0 ">
                 <Image
                   alt="Curi Board UI"
                   src={main_ui}
@@ -48,67 +56,37 @@ export default function Home() {
                   quality={100}
                 />
               </div>
-              <div className="flex gap-5 sm:hidden mt-5 ml-3 overflow-scroll scrollbar-hide">
-                <Image
-                  alt="Curi Board UI"
-                  src={workflow_sm}
-                  width={1007}
-                  height={733}
-                  style={{
-                    maxWidth: "100%",
-                    height: "auto",
-                  }}
-                  quality={100}
-                />
-                <Image
-                  alt="Curi Board UI"
-                  src={dashboard_sm}
-                  width={1085}
-                  height={681}
-                  style={{
-                    maxWidth: "100%",
-                    height: "auto",
-                  }}
-                  quality={100}
-                />
-              </div>
-              <SubscriptionInfo />
             </div>
           </div>
-        </main>
-      </div>
+          <div
+            className={cn(
+              "justify-start items-start flex flex-col gap-2 pb-10 pt-10 pl-10",
+              backgroundStyle2
+            )}
+          >
+            <WorkflowIntroduction />
+          </div>
+          <div className={cn("flex flex-col gap-2")}>
+            <SubscriptionInfo />
+          </div>
+          <div className="px-3 mb-5">
+            <Separator />
+            <div className="text-stone-400 text-sm mt-5">© 2023 RIDEAT.</div>
+            <div className="text-stone-400 text-sm mt-5">대표자 | 강민혁</div>
+            <div className="text-stone-400 text-sm mt-2">
+              email : rideat63@gmail.com
+            </div>
+            <div className="text-stone-400 text-sm">phone : 010-3303-6681</div>
+          </div>
+        </div>
+      </main>
     </>
-  );
-}
-
-const textGradientStyle =
-  "font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-violet-600 to-purple-400";
-
-function LandingHero() {
-  return (
-    <div className="flex flex-col justify-start items-start ">
-      <h1
-        className={cn(
-          "text-3xl sm:text-4xl md:text-5xl font-bold flex flex-col gap-2",
-          textGradientStyle
-        )}
-      >
-        <p className="hidden sm:block">회사의 첫인상을 디자인합니다</p>
-        <p className="sm:hidden">회사의 첫인상을</p>
-        <p className="sm:hidden">디자인합니다</p>
-      </h1>
-      <h3 className="text-stone-700 text-sm sm:text-base md:text-lg font-semibold my-5 md:mb-10">
-        <p>온보딩에 필요한 모든 도구를 제공합니다</p>
-        {/* <p>지금 바로 데모 버전을 마음껏 체험하세요</p> */}
-      </h3>
-      <DemoLinkButton />
-    </div>
   );
 }
 
 function LandingNavigation() {
   return (
-    <div className="flex justify-between my-6">
+    <div className="w-full flex justify-between my-6">
       <NavigationMenu>
         <NavigationMenuList>
           <NavigationMenuItem className="ml-4 sm:ml-8 md:ml-12">
@@ -129,16 +107,78 @@ function LandingNavigation() {
   );
 }
 
+const textGradientStyle = `font-extrabold text-transparent bg-clip-text bg-gradient-to-r`;
+
+function LandingHero() {
+  return (
+    <div className="flex flex-col justify-start items-start ">
+      <h1
+        className={cn(
+          "text-3xl sm:text-4xl md:text-5xl font-bold flex flex-col gap-2",
+          textGradientStyle,
+          "from-violet-600 to-purple-400"
+        )}
+      >
+        <p className="hidden sm:block">회사의 첫인상을 디자인합니다</p>
+        <p className="sm:hidden">회사의 첫인상을</p>
+        <p className="sm:hidden">디자인합니다</p>
+      </h1>
+      <h3 className="text-stone-700 text-sm sm:text-base md:text-lg font-semibold my-5 md:mb-10">
+        <p>온보딩에 필요한 모든 도구를 제공합니다</p>
+      </h3>
+      <DemoLinkButton />
+    </div>
+  );
+}
+
 function DemoLinkButton({ className }: { className?: string }) {
   return (
-    <Link
-      href="/workspace/workflow"
-      className={cn("mr-6 sm:mr-10 md:mr-20", className)}
-    >
-      <Button className="w-28 h-10 text-sm md:w-40 md:h-12 md:text-xl font-bold bg-violet-500 hover:bg-violet-700">
+    <Link href="/workspace/workflow" className={"mr-6 sm:mr-10 md:mr-20"}>
+      <Button
+        className={cn(
+          "w-28 h-10 text-sm md:w-40 md:h-12 md:text-xl font-bold bg-violet-500 hover:bg-violet-700",
+          className
+        )}
+      >
         체험하기
       </Button>
     </Link>
+  );
+}
+
+function WorkflowIntroduction() {
+  return (
+    <div className="flex flex-col justify-start items-start ">
+      <h1
+        className={cn(
+          "text-2xl sm:text-3xl md:text-4xl font-bold flex flex-col gap-2",
+          textGradientStyle,
+          "from-blue-500 to-blue-300"
+        )}
+      >
+        <p className="hidden sm:block">온보딩 프로세스를 쉽게 설계하세요</p>
+        <p className="sm:hidden">온보딩 프로세스를</p>
+        <p className="sm:hidden">쉽게 설계하세요</p>
+      </h1>
+      <h3 className="text-stone-700 text-sm sm:text-base md:text-lg font-semibold my-5 md:mb-10">
+        <p>AI 어시스턴트를 활용하여 컨텐츠를 빠르게 제작하세요</p>
+        {/* <p>지금 바로 데모 버전을 마음껏 체험하세요</p> */}
+      </h3>
+      <DemoLinkButton className="bg-blue-500 hover:bg-blue-700" />
+      <div className="mt-5">
+        <Image
+          alt="Curi Board UI"
+          src={editor_main}
+          width={1365}
+          height={814}
+          style={{
+            maxWidth: "100%",
+            height: "auto",
+          }}
+          quality={100}
+        />
+      </div>
+    </div>
   );
 }
 
@@ -165,21 +205,20 @@ function SubscriptionInfo() {
     });
   }, []);
   return (
-    <div className="flex flex-col md:flex-row justify-center items-center gap-5">
-      <SubscriptionCard title="뉴스레터 구독자" count={data.newletterCount} />
-      <SubscriptionCard
-        title="closed beta 신청자"
-        count={data.reservationsCount}
-      />
+    <div className="flex flex-row justify-center items-center gap-5 p-5">
+      <SubscriptionCard title="뉴스레터" count={data.newletterCount} />
+      <SubscriptionCard title="closed beta" count={data.reservationsCount} />
     </div>
   );
 }
 
 function SubscriptionCard({ title, count }: { title: string; count: number }) {
   return (
-    <Card className="w-1/2 min-w-[220px] md:w-64 shadow-xl">
+    <Card className="w-1/2 min-w-[100px] md:w-64 shadow-xl">
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-        <CardTitle className="text-lg font-medium">{title}</CardTitle>
+        <CardTitle className="text-sm sm:text-base md:text-lg font-medium">
+          {title}
+        </CardTitle>
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
@@ -196,7 +235,7 @@ function SubscriptionCard({ title, count }: { title: string; count: number }) {
         </svg>
       </CardHeader>
       <CardContent>
-        <div className="text-2xl font-bold">
+        <div className="text-sm sm:text-lg md:text-xl font-bold">
           {count == 0 ? "집계중" : `+ ${count}`}
         </div>
       </CardContent>
