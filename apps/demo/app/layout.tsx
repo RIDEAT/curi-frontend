9f69010ee19d52f54e468be77be67717c7153925
Kang <rkdalsgur032@unist.ac.kr>
2023-08-10 12:38:11 +0900

    [RID-361][#45][feat] : channel talk

--- apps/demo/app/layout.tsx
+++ apps/demo/app/layout.tsx
@@ -35,13 +35,24 @@ export default function RootLayout({
       </Script>
       <Script id="gtm-script" strategy="afterInteractive">
         {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer', '${GTM_ID}');
-      `}
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+        `}
       </Script>
+      <Script id="channel-script" strategy="afterInteractive">
+        {`
+            (function(){var w=window;if(w.ChannelIO){return w.console.error("ChannelIO script included twice.");}var ch=function(){ch.c(arguments);};ch.q=[];ch.c=function(args){ch.q.push(args);};w.ChannelIO=ch;function l(){if(w.ChannelIOInitialized){return;}w.ChannelIOInitialized=true;var s=document.createElement("script");s.type="text/javascript";s.async=true;s.src="https://cdn.channel.io/plugin/ch-plugin-web.js";var x=document.getElementsByTagName("script")[0];if(x.parentNode){x.parentNode.insertBefore(s,x);}}if(document.readyState==="complete"){l();}else{w.addEventListener("DOMContentLoaded",l);w.addEventListener("load",l);}})();
+            
+            ChannelIO('boot', {
+              "pluginKey": "${process.env.CHANNEL_PLUGIN_KEY}"
+            });
+            
+        `}
+      </Script>
+
       <body suppressHydrationWarning={true}>
         <noscript>
           <iframe
