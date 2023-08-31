6dc8b30a96e2668a4e0babce1155858fbc99a788
Kang <rkdalsgur032@unist.ac.kr>
2023-08-31 15:19:21 +0900

    [RID-389][#137][fix] : front office badge

--- apps/front-office/app/[front-office-id]/components/status-icon.tsx
+++ apps/front-office/app/[front-office-id]/components/status-icon.tsx
@@ -5,31 +5,14 @@ import { Badge, InProgressIcon } from "ui";
 import { getStatusColor } from "../../../lib/util/getStatusColor";
 
 function StatusIcon({ status }: { status: string }) {
-  const [bgColor, setBgColor] = useState("bg-white");
-  const [text, setText] = useState(status);
-
-  useEffect(() => {
-    setBgColor(getStatusColor(status));
-    switch (status) {
-      case "IN_PROGRESS":
-        setText("진행 중");
-        break;
-
-      default:
-        setText("");
-        break;
-    }
-  }, [status]);
-
   switch (status) {
     case "IN_PROGRESS":
       return <InProgressIcon />;
 
     default:
+      return <div>{status}</div>;
       break;
   }
-
-  return <Badge className={`${bgColor} w-fit`}>{text}</Badge>;
 }
 
 export { StatusIcon };
