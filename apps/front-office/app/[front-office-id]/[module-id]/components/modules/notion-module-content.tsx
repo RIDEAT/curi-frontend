38e69bfb6be02c2e07ad7ee838c519a4a9b0b7a1
Kang <rkdalsgur032@unist.ac.kr>
2023-09-04 16:34:47 +0900

    [RID-398][#137][fix] : front-office bug fix

--- apps/front-office/app/[front-office-id]/[module-id]/components/modules/notion-module-content.tsx
+++ apps/front-office/app/[front-office-id]/[module-id]/components/modules/notion-module-content.tsx
@@ -6,17 +6,21 @@ import { ExternalServiceLinkButton } from "./external-service-link-button";
 function NotionModuleContent({ contents }) {
   return (
     <div>
-      <div className="flex flex-col text-base font-medium">
-        <div>노션으로 작성된 문서입니다.</div>
-        <div>아래버튼을 눌러, 노션에서 확인하세요!</div>
-      </div>
       {contents.notionUrl ? (
-        <ExternalServiceLinkButton url={contents.notionUrl}>
-          {getModuleIcon(NOTION_MODULE_VALUE)}
-          <div>노션에서 보기</div>
-        </ExternalServiceLinkButton>
+        <>
+          <div className="flex flex-col text-base font-medium">
+            <div>노션으로 작성된 문서입니다.</div>
+            <div>아래버튼을 눌러, 노션에서 확인하세요!</div>
+          </div>
+          <ExternalServiceLinkButton url={contents.notionUrl}>
+            {getModuleIcon(NOTION_MODULE_VALUE)}
+            <div>노션에서 보기</div>
+          </ExternalServiceLinkButton>
+        </>
       ) : (
-        <LoadingCircle />
+        <Button variant="outline" className="mt-2">
+          노션 링크를 등록하지 않은 모듈입니다.
+        </Button>
       )}
     </div>
   );
