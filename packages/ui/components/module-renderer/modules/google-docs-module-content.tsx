0ebb74a086da600ce385d92d999c140153325414
Kang <rkdalsgur032@unist.ac.kr>
2023-09-04 18:15:27 +0900

    [RID-398][#137][fix] : back-office preview

--- packages/ui/components/module-renderer/modules/google-docs-module-content.tsx
+++ packages/ui/components/module-renderer/modules/google-docs-module-content.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
 import {
   GOOGLE_DOCS_MODULE_VALUE,
   LoadingCircle,
@@ -7,9 +10,21 @@ import { ExternalServiceLinkButton } from "./external-service-link-button";
 import { ModuleDescriptionCard } from "./module-description-card";
 
 function GoogleDocsModuleContent({ contents }: { contents: any }) {
+  const childRef = useRef<HTMLDivElement | null>(null);
+  const [currentParentWidth, setCurrentParentWidth] = useState(0);
+
+  useLayoutEffect(() => {
+    if (childRef.current && childRef.current.parentNode instanceof Element) {
+      const parentWidth = window.getComputedStyle(
+        childRef.current.parentNode
+      ).width;
+      setCurrentParentWidth(Number(parentWidth.replace("px", "")));
+    }
+  }, []);
+
   return (
-    <div>
-      {contents.url && (
+    <div ref={childRef}>
+      {contents.url && currentParentWidth > 700 && (
         <div className="hidden sm:block">
           <iframe src={contents.url} width="100%" height="600px" />
         </div>
