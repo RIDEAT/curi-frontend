9ebe9789b15ef0848aa6cb0111d0cf6ac4ff8c04
Kang <rkdalsgur032@unist.ac.kr>
2023-09-05 20:45:33 +0900

    [env] : fix fb

--- apps/back-office/lib/firebase/firebaseClient.ts
+++ apps/back-office/lib/firebase/firebaseClient.ts
@@ -17,16 +17,16 @@ export const firebaseConfig = {
 let firebaseApp = {} as FirebaseApp;
 let firebaseAuth = {} as Auth;
 
-if (process.env.NEXT_PUBLIC_FIREBASE_API_KEY) {
+if (process.env.FIREBASE_API_KEY) {
   console.log("It is set firebase env!");
 
   const loadedConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
   };
 
   firebaseConfig.apiKey = loadedConfig.apiKey;
