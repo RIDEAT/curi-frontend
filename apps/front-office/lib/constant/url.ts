c74946d479bc735914c5331cdc7b5bcf22ae54fd
Kang <rkdalsgur032@unist.ac.kr>
2023-09-06 10:41:39 +0900

    [env] : front-office env

--- apps/front-office/lib/constant/url.ts
+++ apps/front-office/lib/constant/url.ts
@@ -1,9 +1,8 @@
 // RESOURSE
-export const RESOURSE_API_URL = "https://api.dev.onbird.team";
+export const RESOURSE_API_URL = process.env.NEXT_PUBLIC_RESOURSE_API_URL;
 
 // Front Office
 export const FRONT_OFFICES_PATH = "/front-offices";
 
 // External
-export const SLACK_MEMBER_OAUTH_URL =
-  "https://slack.com/oauth/v2/authorize?scope=chat%3Awrite&amp;user_scope=&amp;redirect_uri=https%3A%2F%2Fview.dev.onbird.team%2Fslack&amp;client_id=5305401263955.5790799264304";
+export const SLACK_MEMBER_OAUTH_URL = `https://slack.com/oauth/v2/authorize?scope=chat%3Awrite&amp;user_scope=&amp;redirect_uri=${process.env.NEXT_PUBLIC_APP_URL}%2Fslack&amp;client_id=5305401263955.5790799264304`;
